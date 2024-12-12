2c742e7f2cb1518fc1f6687f4b6259864ecb0d6e
Correct Username <correct@example.com>
2024-12-12 13:41:00 +0530

    addded

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:5173", // Replace with your frontend's URL
+    origin: "https://reactfiledownload.vercel.app", // Replace with your frontend's URL
     methods: ["GET","POST"], // Allowed methods
   }));
 
